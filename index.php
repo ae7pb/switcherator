fc7866fc0f47de6c6a8443d7a210604a271d7664
tickerator <david@tickerator.org>
2014-08-13 15:28:40 -0600

    starting on it

--- index.php
+++ index.php
@@ -1,61 +1,60 @@
 <style>
-div div {
+    div div {
         width: 200px;
         height: 300px;
         outline: 1px solid;
         float: left;
         margin: 10px;
-}
-div div span.link {
+    }
+    div div span.link {
         position: absolute;
         width: 200px;
         height: 300px;
-}
-span.blue {
+    }
+    span.blue {
         background-color: blue;
-}
-span.red {
+    }
+    span.red {
         background-color: red;
-}
-span.purple {
+    }
+    span.purple {
         background-color: purple;
-}
-span.orange {
+    }
+    span.orange {
         background-color: orange;
-}
-span.green {
+    }
+    span.green {
         background-color: lime;
-}
-span.yellow {
+    }
+    span.yellow {
         background-color: yellow;
-}
-span.orangered {
+    }
+    span.orangered {
         background-color: orangered;
-}
-span.forestgreen {
+    }
+    span.forestgreen {
         background-color: forestgreen;
-}
-span.fuchsia {
+    }
+    span.fuchsia {
         background-color: fuchsia;
-}
-span.crimson {
+    }
+    span.crimson {
         background-color: crimson;
-}
+    }
 </style>
 
 
 <div>
-<?PHP
-
+    <?PHP
 // decide how we are running and do an appropriate new line
-if(php_sapi_name() == "cli")
+    if (php_sapi_name() == "cli")
         $nl = "\n";
-else
+    else
         $nl = "<br/>";
-$db = new SQLite3('myradiodb.php');
+    $db = new SQLite3('myradiodb.php');
 
 // Radio table.  Has the radio, serial numbers and switch capabilities
-$db->exec("CREATE TABLE IF NOT EXISTS radios (
+    $db->exec("CREATE TABLE IF NOT EXISTS radios (
         id int PRIMARY KEY,
         serialNo text,
         description text,
@@ -68,7 +67,7 @@ $db->exec("CREATE TABLE IF NOT EXISTS radios (
 // Switch table.  Which radio it has, switch number on radio, type of switch and details.
 // Types: Binary, PWM Single Color, PWM Color Change, PWM Hue change
 // Details as appropriate
-$db->exec("CREATE TABLE IF NOT EXISTS switches (
+    $db->exec("CREATE TABLE IF NOT EXISTS switches (
         id int PRIMARY KEY,
         radioID int,
         switchNumber int,
@@ -78,7 +77,7 @@ $db->exec("CREATE TABLE IF NOT EXISTS switches (
 // Program table.  Which radio, program number, start time, duration in minutes, 
 // days and which switches it uses.  So it is searchable the days will be
 // Sun Mon Tue Wed Thu Fri Sat
-$db->exec("CREATE TABLE IF NOT EXISTS programs (
+    $db->exec("CREATE TABLE IF NOT EXISTS programs (
         id int PRIMARY KEY,
         radioID int,
         programNumber int,
@@ -92,7 +91,7 @@ $db->exec("CREATE TABLE IF NOT EXISTS programs (
 // the switch stays on, poll time is seconds or 0 for continuous
 // for the program indicate if this is doing various things based on analog info and
 // which RGB colors are affected by analog info
-$db->exec("CREATE TABLE IF NOT EXISTS inputs (
+    $db->exec("CREATE TABLE IF NOT EXISTS inputs (
         id int PRIMARY KEY,
         radioID int,
         inputNumber int,
@@ -101,177 +100,73 @@ $db->exec("CREATE TABLE IF NOT EXISTS inputs (
         duration int,
         pollTime int
         )");
+    /*
+     * Time limit table.  This sets up when a program will react to an input
+     * Start time, stop time. Days is same format as programs
+     */
+    $db->exec("CREATE TABLE IF NOT EXISTS timeLimits (
+        id int PRIMARY KEY,
+        radioID int,
+        limitNumber int,
+        startTime text,
+        stopTime text,
+        days text
+        )");
 
+//  TODO: colorful layout, switches side by side showing how many it has and if they are programmed or not.
 
-
-
-echo '<form>Enter a response:<input name="testInput"/><input type="submit"/></form>';
-    
-
+    function textExec($command) {
+        echo '<form>Enter a response:<input name="testInput"/><input type="submit"/><input type=hidden value="' . $command .
+        '"/></form>';
+    }
 
 //getStatus("100001");
+    function transmit($command) {
+        $returnArray = array();
+        while (count($returnArray) < 2):
+            $returnArray = array();
+            exec($command, $returnArray);
+            // make sure we got something
+            usleep(10000);
+            $attempts++;
+            if ($attempts == 10)
+                return false;
+        endwhile;
+        return $returnArray;
+    }
 
-
-function getStatus($serialNum) {
-        global $nl,$db;
-        if(strlen($serialNum) < 6)
-                return "invalid serial number$nl";
+    function getRadioInfo($serialNum) {
+        global $nl, $db;
+        if (strlen($serialNum) < 6)
+            return "invalid serial number$nl";
         $attempts = 0;
-		// Read a general status
-		$command = "nrfcl z" . $serialNum . "gs";
-		$returnArray = array();
-        while(count($returnArray)<5):
-			$returnArray = array();
-			exec($command, $returnArray);
-			// make sure we got something
-			usleep(10000);
-			$attempts++;
-			if($attempts == 10) break;
-		endwhile;
-        $sql = "SELECT * FROM radios WHERE serialNo = '$serialNum'";
-        $results = $db->query($sql);
-        $realResult = $results->fetchArray();
-        if($realResult === false)
-			$radioArray = array('programs'=>array(), 'switches'=>array(), 'inputs'=>array());
+        // Read a general status
+        $command = "nrfcl z" . $serialNum . "gs";
+        if(!$returnArray = transmit($command)) {
+            echo "Cannot reach radio.  Sorry.";
+            exit;
+        }
         
-		$radioArray = parseGeneralStatus($returnArray,$radioArray);
-
-		unset($returnArray);
-		usleep(10000);
-		radioAttempt2:
-		// Read the switch display
-		$attempts = 0;
-		$command = "nrfcl z" . $serialNum . "sd";
-		$returnArray = array();
-		while(count($returnArray) < 2):
-			$returnArray = array();
-			exec($command,$returnArray);
-			usleep(10000);
-			$attempts++;
-			if($attempts == 10) break;
-		endwhile;
-		$radioArray = parseSwitchDisplay($returnArray,$radioArray);
-        var_dump($returnArray);
-        echo "<pre>";
-        print_r($radioArray);
-        echo "</pre>";
-
-}
-
-/*
- * Takes in the general status
- * and parses the results
- * Pass in the radioArray which gets passed back after
- */
-function parseGeneralStatus($returnArray,$radioArray) {
-	global $nl, $db;
-	$programs = $radioArray['programs'];
-	$switches = $radioArray['switches'];
-	$inputs = $radioArray['inputs'];
-	foreach($returnArray as $line) {
-		if(strpos($line,"..ok") !== false) {
-			echo "ok$nl";
-		} else if (strpos($line,"S#") === 0) {
-			$time = date("Y/m/d H:i:s",strtotime(substr($line, 11)));
-			echo $time.$nl;
-		} else if (strpos($line,"Progs") === 0) {
-			$aString = str_split(substr($line,5),3);
-			foreach($aString as $char) {
-				$itemNum = intval(substr($char,0,2));
-				if(substr($char,2,1) == "y")
-					$programs[$itemNum]['programmed'] = true;
-				else
-					$programs[$itemNum]['programmed'] = false;
-			}
-		} else if (strpos($line,"SwOn") === 0) {
-			$aString = str_split(substr($line,5),3);
-			foreach($aString as $char) {
-				$itemNum = intval(substr($char,0,2));
-				if(substr($char,2,1) == "y")
-					$switches[$itemNum]['turnedOn'] = true;
-				else
-					$switches[$itemNum]['turnedOn'] = false;
-			}
-		} else if (strpos($line,"Sw") === 0) {
-			$aString = str_split(substr($line,2),3);
-			foreach($aString as $char) {
-				$itemNum = intval(substr($char,0,2));
-				if(substr($char,2,1) == "y")
-					$switches[$itemNum]['programmed'] = true;
-				else
-					$switches[$itemNum]['programmed'] = false;
-			}
-		} else if (strpos($line,"In") === 0) {
-			$aString = str_split(substr($line,2),3);
-			foreach($aString as $char) {
-				$itemNum = intval(substr($char,0,2));
-				if(substr($char,2,1) == "y")
-					$inputs[$itemNum]['programmed'] = true;
-				else
-					$inputs[$itemNum]['programmed'] = false;
-			}
-		}
-		
-	}
-	$radioArray['programs'] = $programs;
-	$radioArray['switches'] = $switches;
-	$radioArray['inputs'] = $inputs;
-	return $radioArray;
-}
-
-function parseSwitchDisplay($returnArray,$radioArray) {
-	$switches = $radioArray['switches'];
-	foreach($returnArray as $line) {
-		if (strpos($line,"#") === 0) {
-			$aString = str_split($line,6);
-			foreach($aString as $char) {
-				$itemNum = intval(substr($char,1,2));
-				$switches[$itemNum]['programmed']= false;
-				$switches[$itemNum]['type'] = false;
-				$switches[$itemNum]['pin'] = false;
-				$switches[$itemNum]['direction'] = false;
-				switch (substr($char,3)){
-					case "COc":
-						$switches[$itemNum]['type'] = 'colorChange';
-						break;
-					case "Brt":
-						$switches[$itemNum]['type'] = 'brightnessSetting';
-						break;
-					case "PWl":
-					case "PWf": // had a bug
-						$switches[$itemNum]['type'] = 'PWM';
-						$switches[$itemNum]['direction'] = 'low';
-						break;
-					case "PWh":
-						$switches[$itemNum]['type'] = 'PWM';
-						$switches[$itemNum]['direction'] = 'high';
-						break;
-					default:
-						if(substr($char,3,1) == '?')
-							break;
-						$switches[$itemNum]['pin'] = substr($char,3,2);
-						if(substr($char,5,1) == 'H')
-							$switches[$itemNum]['direction'] = "high";
-						else
-							$switches[$itemNum]['direction'] = "low";
-				}
-			}
-		}
-	}
-	$radioArray['switches'] = $switches;
-	return $radioArray;
-}
-
-
-?>
-        <div><a href="#"><span class="link blue"></span></a></div>
-        <div><a href="#"><span class="link red"></span></a></div>
-        <div><a href="#"><span class="link purple"></span></a></div>
-        <div><a href="#"><span class="link orange"></span></a></div>
-        <div><a href="#"><span class="link green"></span></a></div>
-        <div><a href="#"><span class="link yellow"></span></a></div>
-        <div><a href="#"><span class="link orangered"></span></a></div>
-        <div><a href="#"><span class="link forestgreen"></span></a></div>
-        <div><a href="#"><span class="link fuchsia"></span></a></div>
-        <div><a href="#"><span class="link crimson"></span></a></div>
+        $time = 0;
+        // go through and glean some information.
+        foreach($returnArray as $line) {
+            if(strpos($line,"S#") === 0) {
+                $timeString = substr($line,11,19);
+                $time = strtotime($timeString);
+            }
+        }
+        
+        
+    }
+    ?>
+    <div><a href="#"><span class="link blue"></span></a></div>
+    <div><a href="#"><span class="link red"></span></a></div>
+    <div><a href="#"><span class="link purple"></span></a></div>
+    <div><a href="#"><span class="link orange"></span></a></div>
+    <div><a href="#"><span class="link green"></span></a></div>
+    <div><a href="#"><span class="link yellow"></span></a></div>
+    <div><a href="#"><span class="link orangered"></span></a></div>
+    <div><a href="#"><span class="link forestgreen"></span></a></div>
+    <div><a href="#"><span class="link fuchsia"></span></a></div>
+    <div><a href="#"><span class="link crimson"></span></a></div>
 </div>
