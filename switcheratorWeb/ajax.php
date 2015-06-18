a3ba173dcdc65e3888e97025d6b175da92859e16
tickerator <david@tickerator.org>
2015-06-18 09:49:56 -0600

    first day of working on web

--- switcheratorWeb/ajax.php
+++ switcheratorWeb/ajax.php
@@ -1,6 +1,105 @@
 <?php
 
-$hello = array(array("radioId"=>"hi"),array("radioId"=>"there"));
-echo json_encode($hello);
+global $db;
+$db = new SQLite3('myradiodb.php');
+switch ($_GET['function']) {
+    case "getRadios":
+        echo getRadios();
+        break;
+    case "radioDetails":
+        echo radioDetails($_GET['radioID']);
+        break;
+}
+
+/*
+ * Return the list of radios
+ */
+
+function getRadios() {
+    global $db;
+    $sql = "select * from radios";
+    $result = $db->query($sql);
+    while ($radioInfo[] = $result->fetchArray(SQLITE3_ASSOC)) {
+        
+    }
+    // one too many entries
+    array_pop($radioInfo);
+    return json_encode($radioInfo);
+}
+
+/*
+ * Return the summary of a single radio
+ */
+
+function radioDetails($radioID) {
+    $radioID = intval($radioID);
+    global $db;
+    // check if the radio is valid
+    $sql = "select * from radios where id = :radioID";
+    $statement = $db->prepare($sql);
+    if (!$statement->bindValue(":radioID", $radioID))
+        return (json_encode(array("fail" => $db->lastErrorMsg())));
+    $result = $statement->execute();
+    $row = $result->fetchArray();
+    if ($row == false)
+        return (json_encode(array("fail" => "Invalid radio id")));
+    $radioDetail = $row;
+    
+    // no go through and get all of the details
+    $sql = "select * from switches where radioID = :radioID and switchStuff != 255";
+    $statement = $db->prepare($sql);
+    if (!$statement->bindValue(":radioID", $radioID, SQLITE3_INTEGER))
+        echo $db->lastErrorMsg();
+    $result = $statement->execute();
+    $switches = array();
+    while($row = $result->fetchArray())
+            $switches[] = $row;
+
+    $sql = "select * from programs where radioID = :radioID and time != 65535";
+    $statement = $db->prepare($sql);
+    if (!$statement->bindValue(":radioID", $radioID, SQLITE3_INTEGER))
+        echo $db->lastErrorMsg();
+    $result = $statement->execute();
+    $programs = array();
+    while($row = $result->fetchArray())
+            $programs[] = $row;
+
+    $sql = "select * from inputs where radioID = :radioID and pinStuff != 255";
+    $statement = $db->prepare($sql);
+    if (!$statement->bindValue(":radioID", $radioID, SQLITE3_INTEGER))
+        echo $db->lastErrorMsg();
+    $result = $statement->execute();
+    $inputs = array();
+    while($row = $result->fetchArray())
+            $inputs[] = $row;
+
+    $sql = "select * from colorChanges where radioID = :radioID and (red != 0 or green != 1 or blue != 0)";
+    $statement = $db->prepare($sql);
+    if (!$statement->bindValue(":radioID", $radioID, SQLITE3_INTEGER))
+        echo $db->lastErrorMsg();
+    $result = $statement->execute();
+    $colors = array();
+    while($row = $result->fetchArray())
+            $colors[] = $row;
+
+    $sql = "select * from timeLimits where radioID = :radioID and (startTime != 0 or stopTime != 0 or days != 0)";
+    $statement = $db->prepare($sql);
+    if (!$statement->bindValue(":radioID", $radioID, SQLITE3_INTEGER))
+        echo $db->lastErrorMsg();
+    $result = $statement->execute();
+    $timeLimits = array();
+    while($row = $result->fetchArray())
+            $timeLimits[] = $row;
+
+    $outArray = array(
+        "radio"=>$radioDetail,
+        "switches"=>$switches,
+        "program"=>$programs,
+        "inputs"=>$inputs,
+        "colors"=>$colors,
+        "timeLimits"=>$timeLimits,
+    );
+    return json_encode($outArray);      
+}
 
 ?>
--- switcheratorWeb/switcherator.js
+++ switcheratorWeb/switcherator.js
@@ -1,11 +1,11 @@
 
 $(document).ready(function () {
-    $.get("ajax.php"
-            , {function: "getRadios"}
-            , function(response){
-                radioDivs(response)
-            }
-            , "json");
+    $.get("ajax.php",
+            {function: "getRadios"},
+    function (response) {
+        radioDivs(response)
+    },
+            "json");
     navigationBar(null);
 
 });
@@ -24,18 +24,44 @@ function navigationBar(radioText) {
  * Populate the existing radioDiv element with a new box for each radio
  */
 function radioDivs(response) {
-
-    response.forEach(function(radio) {
-        var output = radio.radioId;
+    response.forEach(function (radio) {
+        var output = radio.name;
         var container = $("#radioList");
-        var radioNum = container.children().length; // change this to radioId when we have data
+        var radioNum = radio.id;
         container.append(
-                "<div id=radio-" + radioNum + " class=radios>"+output+"</div>"
+                "<div id=radio-" + radioNum + " class=radios onclick='radioDetail(" + radioNum + ")'>" + output + "</div>"
                 );
     })
 }
 /*
+ * Get the details of a certain radio
+ */
+function radioDetail(radioNum) {
+    $("#radioList").children('div').each(function () {
+        $(this).hide();
+    })
+    $("#radio-" + radioNum).show();
+    $("#backToRadioList").show();
+    $("#pleaseWaitRadio").show();
+    $.get("ajax.php"
+            , {function: "radioDetails",
+            radioID: radioNum}
+    , function (response) {
+        showRadioDetails(response)
+    }
+    , "json"
+            );
+}
+
+function showRadioDetails(response) {
+    $("#pleaseWaitRadio").hide();
+    if(response.fail !== null)
+        $("#radioDetails").text(response.fail);
+    //TODO: you are here
+}
+
+/*
  * $('.ring-preview').children('img').each(function(i) { 
-    $(this).rotate(ring.stones[i].stone_rotation);
-});
+ $(this).rotate(ring.stones[i].stone_rotation);
+ });
  */
